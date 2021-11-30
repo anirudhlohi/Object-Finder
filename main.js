b99e0ca38fdcbbca79eff30bf64bc65441df75ce
anirudhlohi <74246267+anirudhlohi@users.noreply.github.com>
2021-11-30 19:49:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 Status = "";
 input = "";
 ;
@@ -11,6 +12,32 @@ function setup(){
 
 function draw(){
   image(video,0,0,640,420);
+
+  if(Status != ""){
+    objectDetector.detect(video,gotResults);   
+
+    for(i = 0; i < objects.length ; i++){
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        
+
+        fill("#fc0303");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%" ,objects[i].x+20,objects[i].y+20);
+        noFill();
+        stroke("#fc0303");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        if(objects[i].label == input){
+           document.getElementById("object_status").innerHTML = "Object Detected";
+          
+        }
+       else{
+           document.getElementById("object_status").innerHTML = "Object Not Detected";
+           
+        
+       }
+ }
+}
 }
 
 function start(){
@@ -24,3 +51,13 @@ function modelLoaded(){
   console.log("Model Loaded!");
   Status = true;
 }
+
+function gotResults(error,results){
+  if(error){
+     console.error(error);
+  }
+  else{
+     console.log(results);
+     objects = results;
+  }
+  }
